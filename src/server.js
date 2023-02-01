526dbfa4f1e4e3100a96f68dd8ebe5ec4f2ae692
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-01 09:46:08 +0900

    assigned routes in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import morgan from "morgan";
+import marsTalkRouter from "./routers/marsTalkRouter";
+import marsTubeRouter from "./routers/marsTubeRouter";
+import rootRouter from "./routers/rootRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
@@ -7,6 +11,10 @@ const app = express();
 const logger = morgan("dev");
 
 app.use(logger);
+app.use("/", rootRouter);
+app.use("/users", userRouter);
+app.use("/marstalk", marsTalkRouter);
+app.use("/marstube", marsTubeRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port: ${PORT}🚀`);
