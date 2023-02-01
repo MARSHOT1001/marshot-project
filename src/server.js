21e51bce6a52227e52f102e87c697ab84767365f
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-01 12:41:59 +0900

    connecting to mongo

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import marsTalkRouter from "./routers/marsTalkRouter";
@@ -6,13 +7,14 @@ import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
+
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/marstalk", marsTalkRouter);
