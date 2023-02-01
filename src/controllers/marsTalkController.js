fc3e2d377a21077a9564608f7a832df20fb51df9
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-01 11:16:12 +0900

    marsTalk templates

--- src/controllers/marsTalkController.js
+++ src/controllers/marsTalkController.js
@@ -1,6 +1,12 @@
-export const marsTalk = (req, res) => res.send("MarsTalk");
-export const friends = (req, res) => res.send("Friends");
-export const chats = (req, res) => res.send("Chats");
-export const views = (req, res) => res.send("Views");
-export const shopping = (req, res) => res.send("Shopping");
-export const more = (req, res) => res.send("More");
+export const marsTalk = (req, res) =>
+  res.render("marstalk/home", { pageTitle: "MarsTalk" });
+export const friends = (req, res) =>
+  res.render("marstalk/friends", { pageTitle: "Friends" });
+export const chats = (req, res) =>
+  res.render("marstalk/chats", { pageTitle: "Chats" });
+export const views = (req, res) =>
+  res.render("marstalk/views", { pageTitle: "Views" });
+export const shopping = (req, res) =>
+  res.render("marstalk/shopping", { pageTitle: "Shopping" });
+export const more = (req, res) =>
+  res.render("marstalk/more", { pageTitle: "More" });
