2c4b53245c2f0175d933c50411b59a7c602c8da7
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-01 15:55:55 +0900

    Update marsTubeController.js

--- src/controllers/marsTubeController.js
+++ src/controllers/marsTubeController.js
@@ -1,37 +1,12 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "3 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "5 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
 export const marstube = (req, res) => {
+  Video.find();
   return res.render("marstube/home", { pageTitle: "MarsTube", videos });
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   return res.render("marstube/watch", {
     pageTitle: `Watching ${video.title}`,
     video,
@@ -40,7 +15,6 @@ export const watch = (req, res) => {
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   return res.render("marstube/edit", {
     pageTitle: `Editing ${video.title}`,
     video,
@@ -50,7 +24,6 @@ export const getEdit = (req, res) => {
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/marstube/${id}`);
 };
 
