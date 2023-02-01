61aafbc0ef89a08878633f576b37c407966c51aa
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-01 09:38:57 +0900

    installed morgan

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
+app.use(logger);
 const handleListening = () =>
   console.log(`Server listening on port: ${PORT}🚀`);
 
