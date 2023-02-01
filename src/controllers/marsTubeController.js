10bb497aacedea8892c5b6427bced1ef03cd156e
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-01 16:26:36 +0900

    error
    Route.post() requires a callback function but got a [object Undefined]

--- src/controllers/marsTubeController.js
+++ src/controllers/marsTubeController.js
@@ -1,32 +1,27 @@
 import Video from "../models/Video";
 
 export const marstube = (req, res) => {
-  Video.find();
-  return res.render("marstube/home", { pageTitle: "MarsTube", videos });
+  Video.find({}, (error, videos) => {
+    console.log("errors", error);
+    console.log("videos", videos);
+  });
+  return res.render("marstube/home", { pageTitle: "MarsTube", videos: [] });
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("marstube/watch", {
-    pageTitle: `Watching ${video.title}`,
-    video,
+    pageTitle: `Watching`,
   });
 };
 
-export const getEdit = (req, res) => {
-  const { id } = req.params;
-  return res.render("marstube/edit", {
-    pageTitle: `Editing ${video.title}`,
-    video,
+export const getEdit = (req, res) =>
+  res.render("marstube/edit", {
+    pageTitle: `Editing`,
   });
+export const postUpload = (req, res) => {
+  res.send("/");
 };
-
-export const postEdit = (req, res) => {
-  const { id } = req.params;
-  const { title } = req.body;
-  return res.redirect(`/marstube/${id}`);
-};
-
 export const upload = (req, res) =>
   res.render("marstube/upload", { pageTitle: "Upload" });
 
