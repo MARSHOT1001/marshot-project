b5e4b93f2c76c1b756135d29706780e4c1b0e70b
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-02 15:21:22 +0900

    installed MongoStore

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import marsTalkRouter from "./routers/marsTalkRouter";
