f3f8dc39e994cd07889f4cb0273136814030377f
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-02 11:30:49 +0900

    installed bcrypt
    installed and imported bcrypt to hash the password

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -6,6 +7,12 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, require: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  console.log("User password: ", this.password);
+  this.password = await bcrypt.hash(this.password, 5);
+  console.log("Hashed password: ", this.password);
 });
 
 const User = mongoose.model("User", userSchema);
