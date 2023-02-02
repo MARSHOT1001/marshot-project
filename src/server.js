705c90f39ba1b0f91d398249631e4c41bfbf9bc5
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-02 15:27:30 +0900

    connecting between mongodb and sessions

--- src/server.js
+++ src/server.js
@@ -21,6 +21,9 @@ app.use(
     secret: "Hello!",
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/marstube",
+    }),
   })
 );
 
