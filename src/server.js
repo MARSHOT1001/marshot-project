48f4b5096f425401c244cfe34c188306e2c3e826
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-02 13:03:23 +0900

    installed express-session

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import morgan from "morgan";
-import marsTalkRouter from "./routers/marsTalkRouter";
-import marsTubeRouter from "./routers/marsTubeRouter";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
+import marsTalkRouter from "./routers/marsTalkRouter";
+import marsTubeRouter from "./routers/marsTubeRouter";
 
 const app = express();
 const logger = morgan("dev");
@@ -12,6 +13,14 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
