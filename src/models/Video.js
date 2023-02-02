a2d42819736cc38560963856e9233b60d734d22a
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-02 09:47:29 +0900

    Update Video.js

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,26 +1,18 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-    mixLength: 2,
-    maxLength: 80,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 5,
-    maxLength: 2000,
-  },
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, maxLength: 1500 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
   },
+});
+
+videoSchema.pre("save", async function () {
+  console.log("We are about to save: ", this);
 });
 
 const Video = mongoose.model("Video", videoSchema);
