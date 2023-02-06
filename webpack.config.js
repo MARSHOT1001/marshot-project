7cacf8cde12df9d92b4addcc356fdfe2c7eb4b3d
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-02-06 15:25:54 +0900

    configuring scss

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  plugins: [new MiniCssExtractPlugin()],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "assets", "js"),
@@ -17,6 +18,10 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
